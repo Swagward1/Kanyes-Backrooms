7722d70c76e1395b253c8a71f27d620ce8c1487e
Swagward <521969@chrome.escg.ac.uk>
2022-04-28 16:11:23 +0100

    finish stage 1

--- Assets/NextStage.cs
+++ Assets/NextStage.cs
@@ -7,15 +7,15 @@ public class NextStage : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject prevStage;
     [SerializeField] GameObject nextStage;
+    [SerializeField] GameObject playerSpawn;
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player"))
         {
-            player.transform.position = new Vector3(transform.position.x + 10, transform.position.y + 10, transform.position.z);
+            player.transform.position = playerSpawn.transform.position;
             prevStage.SetActive(false);
             nextStage.SetActive(true);
-            //Debug.Log("mother fucker can you just work for once");
         }
     }
 }
